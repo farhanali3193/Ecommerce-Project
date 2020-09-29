bbce89c393948254c97a1cdf9911f6a406862974
farhanali3193 <61304043+farhanali3193@users.noreply.github.com>
2020-09-28 22:52:44 -0700

    Adding shopPage, collection-preview and collection-item components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import { Link,Route,Switch } from 'react-router-dom';
-
-const HatsPage = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1> HATS </h1>
-    </div>
-  )
-}
-
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
     <div>
       <Switch>   
         <Route exact path='/' component = {HomePage} />
-        <Route exact path='/hats' component = {HatsPage} />
+        <Route  path='/shop' component = {ShopPage} />
       </Switch>
     </div>
   );
